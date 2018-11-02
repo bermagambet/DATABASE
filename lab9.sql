7f4841168cee7dafb5a677e169310b078f71dd44
bermagambet <36257463+bermagambet@users.noreply.github.com>
2018-11-02 13:47:06 +0600

    Update lab9.sql

--- lab9.sql
+++ lab9.sql
@@ -6,5 +6,6 @@ CREATE UNIQUE INDEX salary_delta ON employees (salary);
 SELECT * FROM employees WHERE salary < value1 and salary > value2;
 CREATE INDEX subs_employees ON employees (substring(name from 1 for 4));
 SELECT * FROM employees WHERE substring(name from 1 for 4) = 'abcd';
-
+CREATE INDEX depart_index ON employees (department_id, salary);
+CREATE INDEX budget_index ON departments (department_id, budget);
 SELECT * FROM employees e JOIN departments d ON d.department_id = e.department_id WHERE d.budget > value2 AND e.salary < value2;
