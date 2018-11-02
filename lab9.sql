63f24afb5e8e3f62b4f2e14c0e42542e606d8313
bermagambet <36257463+bermagambet@users.noreply.github.com>
2018-11-02 13:41:33 +0600

    Add files via upload

---
+++ lab9.sql
@@ -0,0 +1,10 @@
+﻿CREATE INDEX name_of_countries ON countries (name);
+SELECT * FROM countries WHERE name = 'string';
+CREATE INDEX name_of_employees ON employees (name, surname);
+SELECT * FROM employees WHERE name = 'string' AND surname = 'string';
+CREATE UNIQUE INDEX salary_delta ON employees (salary);
+SELECT * FROM employees WHERE salary < value1 and salary > value2;
+CREATE INDEX subs_employees ON employees (substring(name from 1 for 4));
+SELECT * FROM employees WHERE substring(name from 1 for 4) = 'abcd';
+
+SELECT * FROM employees e JOIN departments d ON d.department_id = e.department_id WHERE d.budget > value2 AND e.salary < value2;
